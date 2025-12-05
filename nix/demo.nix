a31e106dd21911bf77a879de854f02390f5ccfbb
Noon van der Silk <noon.vandersilk@iohk.io>
2025-12-05 10:21:29 +0000

    Fixups

--- nix/demo.nix
+++ nix/demo.nix
@@ -10,6 +10,22 @@
           log_level = "debug";
 
           processes = {
+	    hydra-tui-alice = {
+              working_dir = "./";
+              command = pkgs.writeShellApplication {
+                name = "alice-tui";
+                text = ''
+                  ${pkgs.hydra-tui}/bin/hydra-tui \
+                    --connect 0.0.0.0:4001 \
+                    --node-socket devnet/node.socket \
+                    --testnet-magic 42 \
+                    --cardano-signing-key devnet/credentials/alice-funds.sk
+                '';
+              };
+              is_foreground = true;
+              depends_on."hydra-node-alice".condition = "process_started";
+            };
+
             hydra-node = {
               log_location = "./devnet/alice-logs.txt";
               command = pkgs.writeShellApplication {
@@ -23,12 +39,9 @@
                     --api-port 4001 \
                     --monitoring-port 6001 \
                     --hydra-signing-key "${inputs.hydra}/demo/alice.sk" \
+                    --cardano-signing-key "${inputs.hydra}/hydra-cluster/config/credentials/alice.sk" \
                     --hydra-scripts-tx-id ''$HYDRA_SCRIPTS_TX_ID \
-                    --cardano-signing-key "${self}/demo/credentials/alice.sk" \
-                    --peer 127.0.0.1:5002 \
-                    --hydra-verification-key "${inputs.hydra}/demo/alice.vk" \
-                    --cardano-verification-key "${self}/demo/credentials/alice.vk" \
-                    --ledger-protocol-parameters "${self}/demo/protocol-parameters.json" \
+                    --ledger-protocol-parameters "${inputs.hydra}/hydra-cluster/config/protocol-parameters.json" \
                     --blockfrost blockfrost-project.txt \
                     --persistence-dir devnet/persistence/alice \
                     --contestation-period 3s
